508b56d1e5b18908dd82ef7252bb5835f9c2163f
aster <galaster@foxmail.com>
2022-06-06 18:32:17 +0800

    Add random select

--- projects/web-saying/src/main.rs
+++ projects/web-saying/src/main.rs
@@ -2,6 +2,8 @@
 #![feature(arbitrary_self_types)]
 
 use dioxus::prelude::*;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 mod components;
 
@@ -12,7 +14,8 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
-    let data = LadySaying::builtin()[0].clone();
+    let data = LadySaying::builtin().choose(&mut thread_rng()).unwrap().clone();
+    // let data = use_state(&cx, || raw.clone());
     cx.render(rsx!(
         Saying { lady: data }
     ))
